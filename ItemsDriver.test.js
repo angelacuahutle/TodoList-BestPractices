918ac3902c2c8f7394dc315f8b36a72de491ca26
franklin <franklinbenjamin032@gmail.com>
2021-12-09 17:42:55 -0400

    add imports

---
+++ ItemsDriver.test.js
@@ -0,0 +1,17 @@
+import { TestWatcher } from 'jest';
+import {  createItem, removeItem, updateItemStatus } from './src/ItemsDriver';
+import * as index from './src/index.js'
+
+class List {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
+
+let listArr = [];
+
+test('Creates a new object and appends it to the array and the DOM', () => {
+
+});
